ca3c138a41dd63cba2f8747be9fb5088c13c66d6
DanielEstivenAD <danifra14once@gmail.com>
2024-06-18 11:52:58 -0500

    ya

--- APP_gestion_aleatoria.dart
+++ APP_gestion_aleatoria.dart
@@ -1,33 +1,63 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  List<String?> aprendices=[];
-  List<String?> listaTemas=[];
-  List<String?> listaPredefinida=['juan','daniel','felipe','alejandro','camilo','alexander','jacobo','mateo','pablo','andres','estiven','tomas','pepe','gabriel','jeferson','samuel','manuel','posada','jilian','yeison','teo'];
-  List<String?> listaTemasPredefinida=['vectores','vectores condicionales','matrices','matrices con metodos','arreglos 3D','vector de objetos'];
-  int option1; 
+  List<String?> aprendices = [];
+  List<String?> listaTemas = [];
+  List<String?> listaPredefinida = [
+    'juan',
+    'daniel',
+    'felipe',
+    'alejandro',
+    'camilo',
+    'alexander',
+    'jacobo',
+    'mateo',
+    'pablo',
+    'andres',
+    'estiven',
+    'tomas',
+    'pepe',
+    'gabriel',
+    'jeferson',
+    'samuel',
+    'manuel',
+    'posada',
+    'jilian',
+    'yeison',
+    'teo'
+  ];
+  List<String?> listaTemasPredefinida = [
+    'vectores',
+    'vectores condicionales',
+    'matrices',
+    'matrices con metodos',
+    'arreglos 3D',
+    'vector de objetos'
+  ];
+  int option1;
   do {
     print("Bienvenido al menu de gestion aleatoria de exposiciones");
-    print("*"*50);
+    print("*" * 50);
     print("");
     print("1. agregar temas");
     print("2. agregar aprendices");
     print("3. utilizar listas predefinidas");
     print("4. salir");
     print("");
-    print("*"*50);
+    print("*" * 50);
     print("");
     print("digite la opcion");
-    option1=int.parse(stdin.readLineSync()!);
-    switch(option1){
+    option1 = int.parse(stdin.readLineSync()!);
+    switch (option1) {
       case 1:
-      break;
+        break;
       case 2:
-      break;
+        break;
       case 3:
-      break;
+        break;
       default:
-      print("bye bye");
+        print("bye bye");
     }
-  } while (option1!=4);
+  } while (option1 != 4);
+  print("hey");
 }
